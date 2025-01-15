7afdad617766913e067752558776fe4aa210743c
focusedaf <andbeyondinfinity66@gmail.com>
2025-01-16 02:07:15 +0530

    updates

--- app.py
+++ app.py
@@ -127,4 +127,4 @@ def analyze_text():
         return jsonify({"error": "An error occurred during analysis."}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
